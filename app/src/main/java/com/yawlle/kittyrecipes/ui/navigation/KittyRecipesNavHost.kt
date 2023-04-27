57fe9a5f56c417f7efed4fd0c25cf61ea3228acd
yawlle <isabella.gabrielle@hotmail.com>
2023-04-27 13:58:53 -0300

    Added NavHost settings to meal types screen.

--- app/src/main/java/com/yawlle/kittyrecipes/ui/navigation/KittyRecipesNavHost.kt
+++ app/src/main/java/com/yawlle/kittyrecipes/ui/navigation/KittyRecipesNavHost.kt
@@ -1,11 +1,15 @@
 package com.yawlle.kittyrecipes.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.yawlle.kittyrecipes.ui.presentation.home.HomeScreen
 import com.yawlle.kittyrecipes.ui.presentation.SplashHomeScreen
+import com.yawlle.kittyrecipes.ui.presentation.mealtype.RecipeScreen
+import com.yawlle.kittyrecipes.ui.presentation.mealtype.RecipeTypeScreen
 
 @Composable
 fun KittyRecipesNavHost(navController: NavHostController) {
@@ -18,8 +22,30 @@ fun KittyRecipesNavHost(navController: NavHostController) {
         }
 
         composable(Routes.Home.name) {
+            val selectedRecipeType = remember { mutableStateOf("") }
             HomeScreen(
+                navigateToRecipeTypeScreen = { recipeType ->
+                    selectedRecipeType.value = recipeType
+                    navController.navigate(Routes.RecipeType.name + "/$recipeType")
+                }
             )
+        }
+
+        composable(Routes.RecipeType.name + "/{recipeType}") { backStackEntry ->
+            val selectedRecipe = remember { mutableStateOf("") }
+            val recipeType = backStackEntry.arguments?.getString("recipeType")
+            RecipeTypeScreen(
+                navigateToRecipeScreen = { recipe ->
+                    selectedRecipe.value = recipe
+                    navController.navigate(Routes.Recipe.name + "/$recipe")
+                },
+                recipeType = recipeType
+            )
+        }
+
+        composable(Routes.Recipe.name + "/{recipe}") { backStackEntry ->
+            val recipe = backStackEntry.arguments?.getString("recipe")
+            RecipeScreen(recipe = recipe)
         }
     }
 
