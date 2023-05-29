3979cc2cd31357ab6d0f9c62670bef410f4f573a
yawlle <isabella.gabrielle@hotmail.com>
2023-05-29 11:00:29 -0300

    Added AsyncImage with ImageRequest instead just AsyncImage.

--- app/src/main/java/com/yawlle/kittyrecipes/ui/component/home/HorizontalCards.kt
+++ app/src/main/java/com/yawlle/kittyrecipes/ui/component/home/HorizontalCards.kt
@@ -1,5 +1,6 @@
 package com.yawlle.kittyrecipes.ui.component.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -12,21 +13,25 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import com.yawlle.kittyrecipes.R
 import com.yawlle.kittyrecipes.domain.model.Recipe
-import com.yawlle.kittyrecipes.ui.theme.PrimaryColor
+import com.yawlle.kittyrecipes.ui.theme.SecondaryColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HorizontalCards(
     items: List<Recipe>,
     modifier: Modifier,
+    navigateToRecipeScreen: (recipeId: Int) -> Unit,
 ) {
     LazyRow(
-        modifier = modifier.background(PrimaryColor),
+        modifier = modifier,
         contentPadding = PaddingValues(horizontal = 3.dp),
         horizontalArrangement = Arrangement.spacedBy(10.dp),
     ) {
@@ -36,20 +41,27 @@ fun HorizontalCards(
                     .clip(RoundedCornerShape(8.dp))
                     .aspectRatio(1f)
                     .clickable {
-
+                        navigateToRecipeScreen(item.id)
                     },
                 elevation = CardDefaults.cardElevation(
                     defaultElevation = 10.dp
                 )
             ) {
-                AsyncImage(
-                    model = item.image,
+                val painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                    .data(item.image)
+                    .size(Size.ORIGINAL)
+                    .crossfade(true)
+                    .placeholder(R.drawable.food_placeholder)
+                    .fallback(R.drawable.food_placeholder)
+                    .error(R.drawable.food_placeholder)
+                    .build()
+                )
+                Image(
+                    modifier = modifier.background(color = SecondaryColor),
+                    painter = painter,
                     contentDescription = "Image Carousel",
                     contentScale = ContentScale.Crop,
-                    modifier = modifier
-                        .drawWithContent {
-                            drawContent()
-                        },
                 )
             }
         }
